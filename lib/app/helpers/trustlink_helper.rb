65ddbd09037dff759f80255caa9e2025b118b8f6
Artyom Nikolaev <artyom@a22.in>
2014-02-15 12:02:41 +0800

    Using original_fullpath

--- lib/app/helpers/trustlink_helper.rb
+++ lib/app/helpers/trustlink_helper.rb
@@ -2,7 +2,7 @@ require 'domainatrix'
 
 module TrustlinkHelper
   def trustlink_links
-    options = { links: TrustlinkLink.where(page: request.path) }
+    options = { links: TrustlinkLink.where(page: request.original_fullpath) }
     if TrustlinkConfig.bot_ips.include?(request.remote_addr)
       options.merge!(start: TrustlinkConfig.start_code)
       options.merge!(stop:  TrustlinkConfig.stop_code)
