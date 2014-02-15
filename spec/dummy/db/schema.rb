216fd93e9aac47729330da742011e253ef78b9b8
Artyom Nikolaev <artyom@a22.in>
2014-02-15 12:04:12 +0800

    schema added

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208061904) do
+ActiveRecord::Schema.define(version: 20140214155843) do
 
   create_table "trustlink_configs", force: true do |t|
     t.string "name"
